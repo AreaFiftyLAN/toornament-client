520a492a1507caf2aad3729ac40327ad5beb0549
Brenton Poke <akioserano@hotmail.com>
2017-05-03 19:22:49 -0400

    Minor change to endpoint string

--- src/main/java/ch/wisv/toornament/concepts/Disciplines.java
+++ src/main/java/ch/wisv/toornament/concepts/Disciplines.java
@@ -19,14 +19,14 @@ public class Disciplines extends Concept {
 
     public List<Discipline> getDisciplines() throws IOException {
         // TODO: PLACEHOLDER
-        Request request = client.getAuthenticatedRequestBuilder().get().url("https://api.toornament.com/v1"+endpoint).build();
+        Request request = client.getAuthenticatedRequestBuilder().get().url("https://api.toornament.com/v1/disciplines").build();
          String responseBody = client.executeRequest(request).body().string();
         return mapper.readValue(responseBody, mapper.getTypeFactory().constructCollectionType(List.class, Discipline.class));
     }
 
     public DisciplineDetails getDiscipline(String id) throws IOException {
         // TODO: PLACEHOLDER
-        Request request = client.getAuthenticatedRequestBuilder().get().url("https://api.toornament.com/v1"+endpoint+ id ).build();
+        Request request = client.getAuthenticatedRequestBuilder().get().url("https://api.toornament.com/v1/disciplines"+ id ).build();
          String responseBody = client.executeRequest(request).body().string();
          
         return mapper.readValue(responseBody, mapper.getTypeFactory().constructCollectionType(List.class, DisciplineDetails.class));
