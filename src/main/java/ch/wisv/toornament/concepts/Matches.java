0664e24222a577c42861d65e7d52340a209a3b97
Sille Kamoen <sille.kamoen@gmail.com>
2017-05-07 18:40:25 +0200

    Add MatchDetails for getMatchById

--- src/main/java/ch/wisv/toornament/concepts/Matches.java
+++ src/main/java/ch/wisv/toornament/concepts/Matches.java
@@ -3,6 +3,7 @@ package ch.wisv.toornament.concepts;
 import ch.wisv.toornament.ToornamentClient;
 import ch.wisv.toornament.exception.ToornamentException;
 import ch.wisv.toornament.model.Match;
+import ch.wisv.toornament.model.MatchDetails;
 import ch.wisv.toornament.model.TournamentDetails;
 import ch.wisv.toornament.model.enums.MatchSort;
 import okhttp3.Request;
@@ -54,6 +55,22 @@ public class Matches extends Concept {
         } catch (IOException e) {
             e.printStackTrace();
             throw new ToornamentException("Got IOExcption getting matches");
+        }
+    }
+
+    public MatchDetails getMatch(String matchId, boolean withGames) {
+        try {
+            Request request = client.getAuthenticatedRequestBuilder()
+                .get()
+                .url("https://api.toornament.com/v1/tournaments/" + tournament.getId()
+                    + "/matches/" + matchId
+                    + "?with_games=" + (withGames ? "1" : "0"))
+                .build();
+            String responseBody = client.executeRequest(request).body().string();
+            return mapper.readValue(responseBody, MatchDetails.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new ToornamentException("Got IOExcption getting match with ID " + matchId);
         }
     }
 }
