dd3f1126494c2255571d302db1d52629c0199895
Brenton Poke <akioserano@hotmail.com>
2017-05-12 22:28:49 -0400

    Trying to fix something

--- src/main/java/ch/wisv/toornament/concepts/Concept.java
+++ src/main/java/ch/wisv/toornament/concepts/Concept.java
@@ -1,16 +1,12 @@
 package ch.wisv.toornament.concepts;
 
-<<<<<<< HEAD
 import com.fasterxml.jackson.databind.ObjectMapper;
-<<<<<<< HEAD
 
 import ch.wisv.toornament.ToornamentClient;
-=======
->>>>>>> parent of 0b5adf4... Revert "Remove Lombok dependency and apply editorconfig"
-=======
+
 import ch.wisv.toornament.ToornamentClient;
 import com.fasterxml.jackson.databind.ObjectMapper;
->>>>>>> refs/remotes/origin/master
+
 
 abstract class Concept {
     ToornamentClient client;
