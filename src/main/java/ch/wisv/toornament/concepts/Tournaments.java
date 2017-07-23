4d5b17cc905c65d714d299043b8fa4f681def34e
Brenton Poke <akioserano@hotmail.com>
2017-07-22 20:24:56 -0400

    Implementing Tournaments by discipline

--- src/main/java/ch/wisv/toornament/concepts/Tournaments.java
+++ src/main/java/ch/wisv/toornament/concepts/Tournaments.java
@@ -14,8 +14,7 @@ import java.io.IOException;
 import java.util.List;
 
 import static ch.wisv.toornament.ToornamentClient.JSON;
-import com.fasterxml.jackson.core.type.TypeReference;
-import java.util.ArrayList;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import okhttp3.HttpUrl;
 
 public class Tournaments extends Concept {
@@ -63,10 +62,9 @@ public class Tournaments extends Concept {
             .build();
         try {
             String responseBody = client.executeRequest(request).body().string();
-            TypeReference<List<Tournament>> mapType = new TypeReference<List<Tournament>>() {};
-            
-            return mapper.readValue(responseBody, mapType);
-
+            mapper.configure(DeserializationFeature.ACCEPT_EMPTY_ARRAY_AS_NULL_OBJECT, true);
+            return mapper.readValue(responseBody, mapper.getTypeFactory().constructCollectionType(List.class,
+                Tournament.class));
         } catch (IOException e) {
             e.printStackTrace();
             throw new ToornamentException("Couldn't retrieve any tournaments under that discipline");
