a338a9080e23fc93498662586020138bfe48f0bf
Brenton Poke <akioserano@hotmail.com>
2017-08-04 12:08:00 -0400

    Revert "Merge branch 'working-branch'"
    This reverts commit f66ac008a57748b2a4c84b211f25760f0cf49671, reversing
    changes made to c24765667ba636ba8483a9e62e08a0a9cd142a3e.

--- src/main/java/ch/wisv/toornament/concepts/Tournaments.java
+++ src/main/java/ch/wisv/toornament/concepts/Tournaments.java
@@ -15,7 +15,6 @@ import java.util.List;
 
 import static ch.wisv.toornament.ToornamentClient.JSON;
 import com.fasterxml.jackson.databind.DeserializationFeature;
-import javafx.util.Pair;
 
 public class Tournaments extends Concept {
 
@@ -27,29 +26,6 @@ public class Tournaments extends Concept {
         Request request = client.getRequestBuilder()
             .get()
             .url("https://api.toornament.com/v1/tournaments")
-            .build();
-        try {
-            String responseBody = client.executeRequest(request).body().string();
-            return mapper.readValue(responseBody, mapper.getTypeFactory().constructCollectionType(List.class,
-                Tournament.class));
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new ToornamentException("Couldn't retrieve tournaments");
-        }
-
-    }
-    
-    public List<Tournament> getTournamentsWithParams(List<Pair<String,String>> paramsList ) {
-        String url = "https://api.toornament.com/v1/tournaments?";
-
-        for (Pair<String, String> params : paramsList) {
-            url += params.getKey() + "=" + params.getValue() + "&";
-        }
-
-        System.out.println(url);
-        Request request = client.getRequestBuilder()
-            .get()
-            .url(url)
             .build();
         try {
             String responseBody = client.executeRequest(request).body().string();
