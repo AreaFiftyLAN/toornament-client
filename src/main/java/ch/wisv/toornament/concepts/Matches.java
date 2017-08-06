6ad0e05113e1af1e40022105779f794c39355130
Sille Kamoen <sille.kamoen@gmail.com>
2017-08-06 12:54:09 +0200

    Convert match query method with MatchQueryBuilder

--- src/main/java/ch/wisv/toornament/concepts/Matches.java
+++ src/main/java/ch/wisv/toornament/concepts/Matches.java
@@ -6,12 +6,13 @@ import ch.wisv.toornament.model.Match;
 import ch.wisv.toornament.model.MatchDetails;
 import ch.wisv.toornament.model.MatchResult;
 import ch.wisv.toornament.model.TournamentDetails;
-import ch.wisv.toornament.model.enums.MatchSort;
+import ch.wisv.toornament.model.request.MatchQueryBuilder;
 import okhttp3.HttpUrl;
 import okhttp3.Request;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 public class Matches extends Concept {
     private TournamentDetails tournament;
@@ -22,34 +23,23 @@ public class Matches extends Concept {
     }
 
     public List<Match> getMatches() {
-        return getMatches(false, null, null, null, MatchSort.SCHEDULE, null, false);
+        return getMatches(new MatchQueryBuilder());
     }
 
-    public List<Match> getMatches(boolean hasResult, Integer stageNumber, Integer groupNumber, Integer roundNumber,
-                                  MatchSort sort, String participantId, boolean withGames) {
+    public List<Match> getMatches(MatchQueryBuilder builder) {
+        Map<String, String> parameterMap = builder.build();
         try {
             HttpUrl.Builder urlBuilder = new HttpUrl.Builder()
                 .scheme("https")
                 .host("api.toornament.com")
-                .addPathSegment("v1/tournaments")
+                .addPathSegment("v1")
+                .addPathSegment("tournaments")
                 .addPathSegment(tournament.getId())
-                .addPathSegment("matches")
-                .addQueryParameter("has_result", hasResult ? "1" : "0");
+                .addPathSegment("matches");
 
-            if (stageNumber != null) {
-                urlBuilder.addQueryParameter("stage_number", String.valueOf(stageNumber));
+            for (Map.Entry<String, String> parameter : parameterMap.entrySet()) {
+                urlBuilder.addQueryParameter(parameter.getKey(), parameter.getValue());
             }
-            if (groupNumber != null) {
-                urlBuilder.addQueryParameter("group_number", String.valueOf(groupNumber));
-            }
-            if (roundNumber != null) {
-                urlBuilder.addQueryParameter("round_number", String.valueOf(roundNumber));
-            }
-            urlBuilder.addQueryParameter("sort", sort.getName());
-            if (participantId != null) {
-                urlBuilder.addQueryParameter("participant_id", participantId);
-            }
-            urlBuilder.addQueryParameter("with_games", withGames ? "1" : "0");
 
             Request request = client.getAuthenticatedRequestBuilder()
                 .get()
